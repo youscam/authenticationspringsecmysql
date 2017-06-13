9ec74524fec09a852ffb135ea6150691b127a8eb
kamal_azzouzi <youscam@outlook.com>
2017-06-13 21:13:30 +0000

    Create project with all dependencies

---
+++ src/main/java/ma/youscam/AuthenticationSpringsecurityMysqlApplication.java
@@ -0,0 +1,12 @@
+package ma.youscam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationSpringsecurityMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationSpringsecurityMysqlApplication.class, args);
+	}
+}
